1a2176538ddb1ec56a2bb130a10949f446c74307
dushyantkumarsingh010783-sudo <dushyantkumarsingh010783@gmail.com>
2026-08-01 21:49:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,13 @@
 /*=====================================================
  SMART FORM ENTERPRISE v5.0
  Configuration
+ File : config.js
 =====================================================*/
 
 
+"use strict";
+
+
 const WEB_APP_URL =
-"https://script.google.com/macros/s/AKfycbxxGcqNCngf6K97DzahEhr6jdEHogTto9uwMT7drxZRJ1AD1FfInDlg10QcBiXBOiR/exec";
 
-
-
-const APP_CONFIG = {
-
-    name : "SMART FORM ENTERPRISE",
-
-    version : "5.0",
-
-    developer : "PM POSHAN MIS",
-
-    api : WEB_APP_URL
-
-};
+"https://script.google.com/macros/s/AKfycbxxGcoqNCngf6K97DzaheHr6jdEHOgTto9uwMT7drxZRJlADlFflnDlg1OQcBiXBOiR/exec";
