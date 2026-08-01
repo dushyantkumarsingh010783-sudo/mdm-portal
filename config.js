ea0537b708ec6d0622a0adb99bcfcd25be15d2d6
dushyantkumarsingh010783-sudo <dushyantkumarsingh010783@gmail.com>
2026-08-01 15:44:26 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+/* ===========================================
+   SMART FORM ENTERPRISE
+   CONFIGURATION
+=========================================== */
+
+const CONFIG = {
+
+    APP_NAME: "SMART FORM ENTERPRISE",
+
+    VERSION: "3.0",
+
+    API_URL: "https://script.google.com/macros/s/AKfycbxxGcoqNCngf6K97DzaheHr6jdEHOgTto9uwMT7drxZRJlADlFflnDlg1OQcBiXBOiR/exec",
+
+    THEME: "government",
+
+    DEBUG: false
+
+};
